95da08f28ce8a6c3248a448ead7f24fee8c0a2c0
Paulo Quinot <pauloquinot@hotmail.com>
2017-08-31 19:32:08 -0300

    Criação do projeto

---
+++ Xamarim/Calc2/Calc2/Calc2/Calc2/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calc2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            float result = 0;
+            switch (operacao.Text)
+            {
+                case ("+"):
+                    result = float.Parse(valor1.Text) + float.Parse(valor2.Text);
+                    break;
+                case ("-"):
+                    result = float.Parse(valor1.Text) - float.Parse(valor2.Text);
+                    break;
+                case ("*"):
+                    result = float.Parse(valor1.Text) * float.Parse(valor2.Text);
+                    break;
+                case ("/"):
+                    result = float.Parse(valor1.Text) / float.Parse(valor2.Text);
+                    break;
+                default:
+                    break;
+                    
+            }
+            lblResult.Text = nome.Text + " o resultado é: " + result.ToString();
+        }
+    }
+}
